5bed040a665c60a4022c85d909d4917b4a931edf
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-10-02 23:46:56 +0600

    filter data according to category

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tocyu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -17,23 +17,31 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const roomsCollection = client.db('airbnb_DB').collection('rooms')
+    const roomsCollection = client.db("airbnb_db").collection("rooms");
 
-    app.get('/rooms', async(req, res)=>{
-        const rooms = await roomsCollection.find().toArray();
-        res.send(rooms)
-    })
+    app.get("/rooms", async (req, res) => {
+      const category = req.query.category;
+
+      let query = {};
+      if (category) {
+        query.category = { $regex: category, $options: "i" };
+      }
+      const rooms = await roomsCollection.find(query).toArray();
+      res.send(rooms);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -41,10 +49,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res)=>{
-    res.send("Airbnb Server is running.")
-})
+app.get("/", (req, res) => {
+  res.send("Airbnb Server is running.");
+});
 
-app.listen(port, ()=>{
-    console.log(`Server is running on ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
